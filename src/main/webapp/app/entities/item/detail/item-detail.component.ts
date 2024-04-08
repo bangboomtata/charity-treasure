20b3cdb5db51c159ee4ac98658a8cf9366fa41dd
Toru Yamaguchi <txy212@student.bham.ac.uk>
2024-04-08 20:23:54 +0100

    item contains shopID. Shops can edit their own items

--- src/main/webapp/app/entities/item/detail/item-detail.component.ts
+++ src/main/webapp/app/entities/item/detail/item-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { AccountService } from 'app/core/auth/account.service';
 import { IItem } from '../item.model';
 import { DataUtils } from 'app/core/util/data-util.service';
 
@@ -12,12 +12,24 @@ import { DataUtils } from 'app/core/util/data-util.service';
 export class ItemDetailComponent implements OnInit {
   item: IItem | null = null;
 
-  constructor(protected dataUtils: DataUtils, protected activatedRoute: ActivatedRoute) {}
+  constructor(protected dataUtils: DataUtils, protected activatedRoute: ActivatedRoute, private accountService: AccountService) {}
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ item }) => {
       this.item = item;
     });
+  }
+
+  getShopID(): boolean {
+    this.accountService.getShop().subscribe(shop => {
+      if (shop !== null) {
+        if (this.item !== null && shop.id === this.item.id) {
+          return true;
+        }
+      }
+      return false;
+    });
+    return false;
   }
 
   byteSize(base64String: string): string {
