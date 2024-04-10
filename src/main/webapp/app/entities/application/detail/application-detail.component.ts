641ed60f2ef793de85b95a889827cb0e5e75a439
Edward Twe <ext320@student.bham.ac.uk>
2024-04-10 02:48:15 +0200

    Add Application Done

--- src/main/webapp/app/entities/application/detail/application-detail.component.ts
+++ src/main/webapp/app/entities/application/detail/application-detail.component.ts
@@ -3,15 +3,18 @@ import { ActivatedRoute } from '@angular/router';
 
 import { IApplication } from '../application.model';
 import { DataUtils } from 'app/core/util/data-util.service';
+import { ApplicationStatus } from 'app/entities/enumerations/application-status.model';
+import { ApplicationService } from '../service/application.service';
 
 @Component({
   selector: 'jhi-application-detail',
   templateUrl: './application-detail.component.html',
+  styleUrls: ['./application-detail.component.scss'],
 })
 export class ApplicationDetailComponent implements OnInit {
   application: IApplication | null = null;
 
-  constructor(protected dataUtils: DataUtils, protected activatedRoute: ActivatedRoute) {}
+  constructor(protected dataUtils: DataUtils, protected activatedRoute: ActivatedRoute, protected applicationService: ApplicationService) {}
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ application }) => {
@@ -29,5 +32,47 @@ export class ApplicationDetailComponent implements OnInit {
 
   previousState(): void {
     window.history.back();
+  }
+
+  acceptApplication(application: IApplication): void {
+    application.applicationStatus = ApplicationStatus.ACCEPTED;
+    this.applicationService.update(application).subscribe(
+      () => {},
+      error => {
+        console.log('Failed to update application status.');
+      }
+    );
+  }
+
+  rejectApplication(application: IApplication): void {
+    application.applicationStatus = ApplicationStatus.REJECTED;
+    this.applicationService.update(application).subscribe(
+      () => {},
+      error => {
+        console.log('Failed to update application status.');
+      }
+    );
+  }
+
+  pendingApplication(application: IApplication): void {
+    application.applicationStatus = ApplicationStatus.PENDING;
+    this.applicationService.update(application).subscribe(
+      () => {},
+      error => {
+        console.log('Failed to update application status.');
+      }
+    );
+  }
+
+  protected isAccepted(application: IApplication) {
+    return application.applicationStatus === ApplicationStatus.ACCEPTED;
+  }
+
+  protected isRejected(application: IApplication) {
+    return application.applicationStatus === ApplicationStatus.REJECTED;
+  }
+
+  protected isPending(application: IApplication) {
+    return application.applicationStatus === ApplicationStatus.PENDING;
   }
 }
--- src/main/webapp/app/entities/application/update/application-update.component.ts
+++ src/main/webapp/app/entities/application/update/application-update.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpResponse } from '@angular/common/http';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { finalize, map } from 'rxjs/operators';
+import { Validators, FormControl } from '@angular/forms';
 
 import { ApplicationFormService, ApplicationFormGroup } from './application-form.service';
 import { IApplication } from '../application.model';
@@ -15,12 +16,33 @@ import { VolunteerPostService } from 'app/entities/volunteer-post/service/volunt
 import { ICustomer } from 'app/entities/customer/customer.model';
 import { CustomerService } from 'app/entities/customer/service/customer.service';
 import { ApplicationStatus } from 'app/entities/enumerations/application-status.model';
+import { AccountService } from 'app/core/auth/account.service';
+import dayjs from 'dayjs/esm';
 
 @Component({
   selector: 'jhi-application-update',
   templateUrl: './application-update.component.html',
+  styleUrls: ['./application-form.scss'],
 })
 export class ApplicationUpdateComponent implements OnInit {
+  mondayNeeded: boolean | undefined;
+  tuesdayNeeded: boolean | undefined;
+  wednesdayNeeded: boolean | undefined;
+  thursdayNeeded: boolean | undefined;
+  fridayNeeded: boolean | undefined;
+  saturdayNeeded: boolean | undefined;
+  sundayNeeded: boolean | undefined;
+
+  morningNeeded: boolean | undefined;
+  afternoonNeeded: boolean | undefined;
+  eveningNeeded: boolean | undefined;
+
+  customerId: number | undefined;
+  postId: number | undefined;
+  postTitle: string | null | undefined;
+
+  termsAccepted = false;
+
   isSaving = false;
   application: IApplication | null = null;
   applicationStatusValues = Object.keys(ApplicationStatus);
@@ -29,6 +51,8 @@ export class ApplicationUpdateComponent implements OnInit {
   customersSharedCollection: ICustomer[] = [];
 
   editForm: ApplicationFormGroup = this.applicationFormService.createApplicationFormGroup();
+  emailControl: FormControl = new FormControl('', [Validators.required, Validators.email]); // FormControl for email with validators
+  //phoneControl: FormControl = new FormControl('', [Validators.required, Validators.pattern(/^\d{10}$/)]); // FormControl for phone with validators
 
   constructor(
     protected dataUtils: DataUtils,
@@ -37,15 +61,137 @@ export class ApplicationUpdateComponent implements OnInit {
     protected applicationFormService: ApplicationFormService,
     protected volunteerPostService: VolunteerPostService,
     protected customerService: CustomerService,
-    protected activatedRoute: ActivatedRoute
+    protected activatedRoute: ActivatedRoute,
+    protected accountService: AccountService,
+    protected router: Router
   ) {}
+
+  clickedTerms(): void {
+    // Implement logic to check if the terms checkbox is checked
+    const checkbox = document.getElementById('termsCheckbox') as HTMLInputElement;
+
+    if (checkbox.checked) {
+      this.termsAccepted = true;
+    } else {
+      this.termsAccepted = false;
+    }
+  }
 
   compareVolunteerPost = (o1: IVolunteerPost | null, o2: IVolunteerPost | null): boolean =>
     this.volunteerPostService.compareVolunteerPost(o1, o2);
 
   compareCustomer = (o1: ICustomer | null, o2: ICustomer | null): boolean => this.customerService.compareCustomer(o1, o2);
 
+  get isMondayNeeded(): boolean {
+    return !!this.mondayNeeded;
+  }
+
+  get isTuesdayNeeded(): boolean {
+    return !!this.tuesdayNeeded;
+  }
+
+  get isWednesdayNeeded(): boolean {
+    return !!this.wednesdayNeeded;
+  }
+
+  get isThursdayNeeded(): boolean {
+    return !!this.thursdayNeeded;
+  }
+
+  get isFridayNeeded(): boolean {
+    return !!this.fridayNeeded;
+  }
+
+  get isSaturdayNeeded(): boolean {
+    return !!this.saturdayNeeded;
+  }
+
+  get isSundayNeeded(): boolean {
+    return !!this.sundayNeeded;
+  }
+
+  get isMorningNeeded(): boolean {
+    return !!this.morningNeeded;
+  }
+
+  get isAfternoonNeeded(): boolean {
+    return !!this.afternoonNeeded;
+  }
+
+  get isEveningNeeded(): boolean {
+    return !!this.eveningNeeded;
+  }
+
   ngOnInit(): void {
+    //set default value
+    this.editForm.patchValue({
+      applicationDate: dayjs(), // Set default to current date
+      applicationStatus: ApplicationStatus.PENDING, // Set default to pending
+    });
+
+    //get details of current logged in user
+    this.accountService.identity().subscribe(account => {
+      if (account) {
+        //get customer based on user id
+        this.accountService.getCustomer().subscribe(customer => {
+          if (customer) {
+            this.customerId = customer.id;
+            // console.log('Customer ID: ', customer.id);
+
+            const castedCustomer = customer
+              ? {
+                  id: customer.id,
+                }
+              : null;
+
+            //set default value of customer
+            this.editForm.patchValue({
+              customer: castedCustomer,
+            });
+          }
+        });
+      }
+    });
+
+    //get the volunteer post that this application is applying
+    this.activatedRoute.queryParams.subscribe(params => {
+      const volunteerPostId = params['volunteerPostId'];
+      this.postId = volunteerPostId;
+
+      if (this.postId) {
+        this.volunteerPostService.findById(this.postId).subscribe(volunteerPost => {
+          if (volunteerPost) {
+            this.postTitle = volunteerPost?.postTitle;
+
+            this.mondayNeeded = !!volunteerPost.monday;
+            this.tuesdayNeeded = !!volunteerPost.tuesday;
+            this.wednesdayNeeded = !!volunteerPost.wednesday;
+            this.thursdayNeeded = !!volunteerPost.thursday;
+            this.fridayNeeded = !!volunteerPost.friday;
+            this.saturdayNeeded = !!volunteerPost.saturday;
+            this.sundayNeeded = !!volunteerPost.sunday;
+
+            this.morningNeeded = !!volunteerPost.morning;
+            this.afternoonNeeded = !!volunteerPost.afternoon;
+            this.eveningNeeded = !!volunteerPost.evening;
+          }
+        });
+
+        const castedPost = volunteerPostId
+          ? {
+              id: volunteerPostId,
+            }
+          : null;
+
+        //make the default value of post to the volunteer post that this application is applying
+        this.editForm.patchValue({
+          post: castedPost,
+        });
+
+        //console.log('POST ID: ' + this.postId);
+      }
+    });
+
     this.activatedRoute.data.subscribe(({ application }) => {
       this.application = application;
       if (application) {
@@ -54,6 +200,11 @@ export class ApplicationUpdateComponent implements OnInit {
 
       this.loadRelationshipsOptions();
     });
+  }
+
+  autoFillPost(volunteerID: number): boolean {
+    //console.log(volunteerID + ": " + (volunteerID.toString() == this.postId?.toString()));
+    return volunteerID.toString() == this.postId?.toString();
   }
 
   byteSize(base64String: string): string {
@@ -93,7 +244,7 @@ export class ApplicationUpdateComponent implements OnInit {
   }
 
   protected onSaveSuccess(): void {
-    this.previousState();
+    this.router.navigate(['/application-success']);
   }
 
   protected onSaveError(): void {
