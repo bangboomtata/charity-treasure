9f707d57200562545066b405a598a86ca24033d1
Toru Yamaguchi <txy212@student.bham.ac.uk>
2024-04-15 16:09:43 +0100

    owner of shop can edit their items

--- src/main/webapp/app/entities/item/detail/item-detail.component.ts
+++ src/main/webapp/app/entities/item/detail/item-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { AccountService } from 'app/core/auth/account.service';
 import { IItem } from '../item.model';
 import { DataUtils } from 'app/core/util/data-util.service';
 
@@ -11,13 +11,43 @@ import { DataUtils } from 'app/core/util/data-util.service';
 })
 export class ItemDetailComponent implements OnInit {
   item: IItem | null = null;
+  editable: boolean = false;
+  Shop: boolean = true;
 
-  constructor(protected dataUtils: DataUtils, protected activatedRoute: ActivatedRoute) {}
+  constructor(protected dataUtils: DataUtils, protected activatedRoute: ActivatedRoute, private accountService: AccountService) {}
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ item }) => {
       this.item = item;
+      if (this.getShopID() === true) {
+        this.editable = true;
+      }
+      if (this.isShop() === true) {
+        this.Shop = false;
+      }
     });
+  }
+
+  isShop(): boolean {
+    this.accountService.getShop().subscribe(shop => {
+      if (shop !== null) {
+        return true;
+      }
+      return false;
+    });
+    return false;
+  }
+
+  getShopID(): boolean {
+    this.accountService.getShop().subscribe(shop => {
+      if (shop !== null) {
+        if (this.item !== null && shop === this.item.shop) {
+          return true;
+        }
+      }
+      return false;
+    });
+    return false;
   }
 
   byteSize(base64String: string): string {
